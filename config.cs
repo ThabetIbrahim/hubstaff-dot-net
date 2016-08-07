d476f350d03c49b2b86cc1e4f09537c682fdcf85
ThabetIbrahim <thabet.ig@gmail.com>
2016-08-07 12:26:36 +0300

    Update config.cs

--- config.cs
+++ config.cs
@@ -2,7 +2,15 @@ namespace config
 {
     class config_class
     { 
-        public string root_folder = Directory.GetCurrentDirectory()+"/"; // hubstaff library directory
+        public string root_folder = Directory.GetCurrentDirectory()+"/";
+
+        public config_class()
+        {
+            if(Path.GetFileName(Directory.GetCurrentDirectory()) != "hubstaff")
+            {
+                root_folder =  Directory.GetCurrentDirectory()+"/hubstaff/";
+            }
+        }
         public string base_url = "https://api.hubstaff.com/v1/";
         public string auth_url = "auth";
         public string users = "users";
